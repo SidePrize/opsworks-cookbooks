d21915878c797929c457ff161ae7cc2305e97727
Brett J Norris <brettjnorris@gmail.com>
2015-09-04 10:35:27 -0700

    Remove redis.yml from sidekiq scripts

--- opsworks_sidekiq/recipes/configure.rb
+++ opsworks_sidekiq/recipes/configure.rb
@@ -22,22 +22,4 @@ node[:deploy].each do |application, deploy|
       File.exists?(deploy[:deploy_to]) && File.exists?("#{deploy[:deploy_to]}/shared/config/")
     end
   end
-
-  template "#{deploy[:deploy_to]}/shared/config/redis.yml" do
-    source "redis.yml.erb"
-    cookbook 'rails'
-    mode "0660"
-    group deploy[:group]
-    owner deploy[:user]
-    variables(
-      :redis => deploy[:redis] || {},
-      :environment => deploy[:rails_env]
-    )
-
-    notifies :run, resources(:execute => "restart Rails app #{application}")
-
-    only_if do
-      File.exists?(deploy[:deploy_to]) && File.exists?("#{deploy[:deploy_to]}/shared/config/")
-    end
-  end
 end
